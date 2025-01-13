00cf162abf4439a7e524511f1d594d7a4b4c8127
GuiNascimento13 <guifnsilva@gmail.com>
2025-01-13 14:48:06 -0300

    Projeto Inicial
    Código contendo INCLUIR, CONSULTAR e DELETAR.

---
+++ registro_ebac.c
@@ -0,0 +1,151 @@
+#include <stdio.h> //biblioteca de cominicação com o usuário 
+#include <stdlib.h> //biblioteca de alocação de espaço em memória
+#include <locale.h> //biblioteca de alocação de texto por região
+#include <string.h> // biblioteca responsável pelas strings
+//bibliotecas devem estar sempre no início do código
+
+
+int registrar() //Defindo a função Registrar
+{
+	setlocale(LC_ALL, "Portuguese"); //Definição de Linguagem
+	
+	// Início da definição de variáveis tipo strings
+	char arquivo[12]; 
+	char cpf[12]; 
+	char nome[20];
+	char sobrenome[20];
+	char atribuicao[15]; 
+	//Fim das variáveis
+	
+	//Início da coleta dos dados a serem registrados	
+	printf("Você escolheu Registrar!\n\n");
+	printf("Digite o CPF:");
+	scanf("%s", cpf);
+	
+	printf("Nome:");
+	scanf("%s", nome);
+	
+	printf("Sobrenome:");
+	scanf("%s", sobrenome);
+	
+	printf("Atribuição:");
+	scanf("%s", atribuicao);
+	
+	strcpy(arquivo, cpf); //Copia as váriaveis strings (não esquecer de criar a variável arquivo)
+		
+	FILE *file; //Cria o arquivo
+	file = fopen(arquivo, "w"); //Comando para escrever o arquivo ("w" escreve e "a" atualiza)
+	fprintf(file, cpf);
+	fprintf(file, "\n");
+	fprintf(file, nome);
+	fprintf(file, "\n");
+	fprintf(file, sobrenome);
+	fprintf(file, "\n");
+	fprintf(file, atribuicao);
+	fclose(file); //Fecha e salva o arquivo na pasta destino
+	//Fim da coleta de dados 
+}
+
+int consultar() //Defindo a função Consultar
+{
+	setlocale(LC_ALL, "Portuguese"); //Definição de Linguagem
+	
+	//Início da definição de variáveis tipo strings
+	char cpf[20];
+	char info[100];
+	//Fim das vaiáveis
+	
+	//Início da consulta
+	printf("Você escolheu Consultar!\n");
+	printf("Digite o CPF:");
+	scanf("%s", cpf);
+	
+	FILE *file;
+	file = fopen(cpf, "r"); //Abre o arquivo para leitura ("r" faz a leitura)
+	
+	if(file == NULL) //Comando de ação para resultado nulo ou inexistente
+	{
+		printf("\n");
+		printf("Registro não encontrado!\n\n");
+	}
+	
+	while(fgets(info, 100, file) != NULL) //Comando para mostrar os dados do arquivo lido
+	{
+		printf("Dados do Registro:");
+		printf("%s", info);
+		printf("\n");
+	}
+	system("pause");
+	//Fim da consulta
+}
+
+int deletar() // Definindo a função Deletar
+{
+	setlocale(LC_ALL, "Portuguese"); //Definição de Linguagem
+	
+	//Início da definição das variáveis tipo strings
+	char cpf[12];
+	//Fim da definição de variáveis
+	
+	//Início da ação Deletar
+	printf("Você escolheu Deletar!\n");
+	printf("Digite o CPF:");
+	scanf("%s", cpf);
+	
+	remove(cpf); //Comando para deletar
+	
+	FILE *file;
+	file = fopen(cpf, "r");
+	
+	if(file == NULL);
+	{
+		printf("Resgistro Deletado!\n");
+		system("pause");
+	}
+	//Fim da ação
+	
+}
+
+int main()
+{
+	int opcao=0; //Definição de variáveis
+	int loop=1;
+	
+	for(loop=1;loop=1;) // Comando para sempre retornar ao MENU Inicial
+	{
+		system("cls"); //Comando para limpar tela
+	
+		setlocale(LC_ALL, "Portuguese"); //Definição de Linguagem
+	
+		printf("\t Registro EBAC \n\n "); // Início do Menu
+		printf("Escolha uma das opções do menu abaixo:\n\n ");
+		printf("\t1 - Registrar\n ");	
+		printf("\t2 - Consultar\n ");
+		printf("\t3 - Deletar\n\n ");
+		printf("Selecione uma das opções e tecle ENTER:"); 
+	
+		scanf("%d", &opcao); // Armazenamento de Ação do Usuário (Pressionar Tecla)
+	
+		system("cls"); // Fim do Menu
+	
+			switch(opcao) // Início da Seleção (Resposta à Ação do Usuário)	
+				{
+					case 1:
+					registrar();
+					break;
+					
+					case 2:
+					consultar();
+					break;
+					
+					case 3:
+					deletar();
+					break;
+					
+					default:
+					printf("Opção Inválida!\n");
+					system("pause");
+					break;		
+					}//Fim da Seleção
+	}	
+}
